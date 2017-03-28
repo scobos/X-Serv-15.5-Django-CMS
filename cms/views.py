97ca7a1ee9bbd9d7a5731204246b23363946abc0
Sergio Cobos <SergioCobos@MacBook-Pro-de-Cobos.local>
2017-03-28 21:14:24 +0200

    Ejercicio 15.5

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from cms.models import Pages
+
 
 # Create your views here.
+
+def pagina(request, identificador):
+
+    try:
+        pag = Pages.objects.get(id = int(identificador))
+        respuesta = pag.page
+    except Pages.DoesNotExist:
+        respuesta = "El nombre no está en la base de datos"
+    return HttpResponse(respuesta)
+
+def mostrar_Info(request):
+    lista = Pages.objects.all()
+    respuesta = "<ol>"
+    for pag in lista:
+        respuesta += '<li><a href="' + str(pag.id) + '">' + pag.name + '</a>'
+    respuesta += "</ol>"
+    return HttpResponse(respuesta)
